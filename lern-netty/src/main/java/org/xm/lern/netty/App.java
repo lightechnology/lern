0e8c10726762f39713fa0a4e2d3a97e294fc1b23
姚杰鸿 <yaojiehong@hongte.info>
2018-02-10 08:35:24 +0800

    工程初始化

---
+++ lern-netty/src/main/java/org/xm/lern/netty/App.java
@@ -0,0 +1,13 @@
+package org.xm.lern.netty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ lern-spring/src/main/java/org/xm/lern/spring/App.java
@@ -0,0 +1,13 @@
+package org.xm.lern.spring;
+
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class).web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
